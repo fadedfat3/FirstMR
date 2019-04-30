3446e36dae6bd1b38d2867675a10563766298c89
fadedfat3 <1743723571@qq.com>
2019-04-30 17:53:14 +0800

    init

---
+++ src/main/java/MaxTemperature.java
@@ -0,0 +1,39 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class MaxTemperature {
+    static {
+    try {
+        System.load("E:/software/hadoop-3.1.1/bin/hadoop.dll");
+    } catch (UnsatisfiedLinkError e) {
+        System.err.println("Native code library failed to load.\n" + e);
+        System.exit(1);
+    }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: MaxTemperature <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Job job = Job.getInstance();
+        job.setJarByClass(MaxTemperature.class);
+        job.setJobName("Max temperature");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(MaxTemperatureMapper.class);
+        job.setReducerClass(MaxTemperatureReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.waitForCompletion(true);
+        System.exit(0);
+    }
+}
